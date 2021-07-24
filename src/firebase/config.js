c43203180f5183f1790db110053830fcd964d84e
ksushant6566 <ksushant6566@gmail.com>
2021-07-24 13:44:25 +0530

    integrating mixpanel

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDpob3WH0iWiSbpVyL-dYzRZxpyrbMhmbw",
+    authDomain: "shiyacheen.firebaseapp.com",
+    projectId: "shiyacheen",
+    storageBucket: "shiyacheen.appspot.com",
+    messagingSenderId: "1089692910756",
+    appId: "1:1089692910756:web:8a618496febd72c2579231"
+};
